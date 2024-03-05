38266c514f7cbd65a56c9aae8b934ca89846f95f
Robert Marina <marina.robert1508@gmail.com>
2024-03-05 15:24:29 +0100

    delete comment

--- src/fr/hymaia/exo4/no_udf.py
+++ src/fr/hymaia/exo4/no_udf.py
@@ -11,14 +11,14 @@ df = spark.read.csv("./src/resources/exo4/sell.csv", header=True)
 
 
 df = df.withColumn("category_name", when(col("category") < 6, "food").otherwise("furniture"))
-"""
+
 df = df.withColumn("total_price_per_category_per_day",
                    sum("price").over(Window.partitionBy("date", "category_name")))
 
 window_spec = Window.partitionBy("category_name").orderBy("date").rowsBetween(-29, 0)
 df = df.withColumn("total_price_per_category_per_day_last_30_days",
                    sum("price").over(window_spec))
-"""
+
 df.show(truncate=False)
 
 spark.stop()
